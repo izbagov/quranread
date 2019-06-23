42667821d219257c221a4c438dd31e5fa484da31
Ramazan Izbagov <izbagov@gmail.com>
2019-06-24 01:58:15 +0300

    Fix word play and add icons play and pause

--- src/components/Verse/index.js
+++ src/components/Verse/index.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
 import s from './Verse.module.scss';
+import { ReactComponent as IconPlay } from '../../assets/play.svg';
+import { ReactComponent as IconPause } from '../../assets/pause.svg';
 
 const Verse = verse => {
-  const [clicked, setClicked] = useState(null);
-  // const [wordActive, setClicked] = useState(null);
+  const [clickedWord, setClickedWord] = useState(null);
+  const [play, setPlay] = useState(false);
+
   const wordPlayAudio = (wordAudio, id) => {
-    if (wordAudio) {
-      setClicked(id);
+    if (wordAudio && !clickedWord) {
+      setClickedWord(id);
       const audio = new Audio(wordAudio);
       audio.play();
-      audio.addEventListener('ended', () => setClicked(null));
+      audio.addEventListener('ended', () => setClickedWord(null));
     }
   };
+
+  const playVerse = () => {
+    setPlay(!play);
+  };
+
+  console.log(verse);
 
   return (
     <div className={s.container}>
@@ -20,15 +29,18 @@ const Verse = verse => {
         {verse.words.map(word => (
           <div
             key={word.id}
-            className={`${word.class_name} ${word.char_type} ${s.word} ${clicked ? s.active : ''} ${
-              clicked === word.id ? s.activeWord : ''
-            }`}
+            className={`${word.class_name} ${word.char_type} ${s.word} ${
+              clickedWord ? s.active : ''
+            } ${clickedWord === word.id ? s.activeWord : ''}`}
             dangerouslySetInnerHTML={{ __html: word.code }}
             onClick={() => wordPlayAudio(word.audio.url, word.id)}
           />
         ))}
       </div>
       <div className={s.translation}>{verse.translations[0].text}</div>
+      <div className={s.playAyat} onClick={() => playVerse()}>
+        {!play ? <IconPlay /> : <IconPause />}
+      </div>
     </div>
   );
 };
